fcc6b919e476d3303a60421a75782acd44d90215
Aleksandr Kostiuk <kosskill2011@gmail.com>
2019-01-29 22:07:48 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-let mainNav = document.getElementById('js-nav');
-let navToggle = document.getElementById('js-toggle');
-
-navToggle.addEventListener('click', function () {
-    
-    mainNav.classList.toggle('js-active');
-});
